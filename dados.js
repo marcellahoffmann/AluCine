e3987464c95647e7511c6999c001a260526daccd
Marcella Hoffmann <marcellahoffmann31@gmail.com>
2024-09-06 18:48:55 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,39 @@
+let dados = [
+    {
+        titulo: "Divertida Mente 2 ⭐⭐⭐⭐4.7",
+        descricao: "Divertida Mente 2 é a aguardada sequência do sucesso da Pixar que nos leva de volta à mente da agora adolescente Riley. Com a chegada da adolescência, a mente de Riley passa por uma grande transformação, e sua Sala de Controle, onde as emoções Alegria, Tristeza, Raiva, Medo e Nojinho comandam, está prestes a ser demolida para dar lugar a algo totalmente novo: novas emoções.Com a chegada de emoções como Ansiedade, Inveja, Tédio e Vergonha, a dinâmica da Sala de Controle é completamente alterada. As emoções originais se sentem desorientadas e precisam aprender a lidar com essa nova realidade. A jornada de Riley se torna ainda mais complexa, com os desafios típicos da adolescência se misturando com as novas emoções que surgem em sua mente.",
+        link: "https://www.disneyplus.com/pt-br/movies/divertida-mente-2-inside-out-2/6MeZYf9JkFii",
+        tags: "divertida mente 2 riley disney"
+    },
+    {
+        titulo: "Planeta dos Macacos ⭐⭐⭐ 3.6",
+        descricao: "A saga de Planeta dos Macacos é uma jornada cinematográfica que explora a complexa relação entre humanos e macacos em um futuro pós-apocalíptico, onde os primatas evoluíram e se tornaram a espécie dominante. A franquia, que se iniciou nos anos 1960, passou por diversas reinterpretações ao longo das décadas, mas sempre manteve como tema central a luta pela sobrevivência e a busca por identidade em um mundo em constante mutação.. Aos poucos anos, ela já coleciona títulos e é considerada uma das maiores ginastas da história do Brasil.",
+        link: "https://www.disneyplus.com/pt-br/movies/o-planeta-dos-macacos/627WrK7hgKhw",
+        tags: "planeta macacos "
+    },
+    {
+        titulo: "Um Lugar Silencioso ⭐⭐⭐⭐ 4.3",
+        descricao: "Um Lugar Silencioso é um thriller de terror pós-apocalíptico que prende a respiração do público do início ao fim. Em um mundo devastado por criaturas cegas, mas com uma audição extremamente aguçada, a menor ruído pode significar a morte. A história se concentra em uma família que luta para sobreviver nesse cenário sombrio. Para se protegerem das criaturas, eles precisam viver em completo silêncio. A comunicação se torna um desafio, e cada movimento é calculado para não atrair a atenção dos predadores.",
+        link: "https://www.netflix.com/br/title/80213226",
+        tags: "lugar silencioso"
+    },
+    {
+        titulo: "Friends ⭐⭐⭐⭐⭐ 4.9",
+        descricao: "Friends é uma sitcom americana que conquistou o coração de milhões de espectadores ao redor do mundo. A série acompanha a vida de seis amigos, Rachel, Monica, Phoebe, Joey, Chandler e Ross, que moram em um apartamento em Nova York. Juntos, eles vivenciam as alegrias e as dificuldades da vida adulta, desde o amor e o trabalho até as amizades e os sonhos.A trama se desenrola em torno das aventuras e desventuras desse grupo de amigos, que se conhecem desde a faculdade. Cada um deles possui uma personalidade marcante e peculiar, o que gera situações cômicas e emocionantes a cada episódio.",
+        link: "https://www.max.com/br/pt?gad_source=1&gclid=Cj0KCQjw0Oq2BhCCARIsAA5hubWqS-szYUfVdJMHciY3TlUMi2Athr3AtFJy1GXXRhnoySQeHkVspTYaAqw-EALw_wcB&gclsrc=aw.ds",
+        tags: "friends rachel monica ross chandler joey phoebe"
+    },
+    {
+        titulo: "How I Met Your Mother ⭐⭐⭐⭐⭐ 4.9",
+        descricao: "How I Met Your Mother é uma sitcom americana que conquistou o coração de milhões de espectadores ao redor do mundo. A série conta a história de Ted Mosby, um arquiteto romântico que, em 2030, narra aos seus dois filhos adolescentes como conheceu a mãe deles. A trama se desenrola em flashbacks, mostrando as aventuras de Ted e seus quatro melhores amigos: Marshall Eriksen, um advogado apaixonado e leal; Lily Aldrin, uma professora de jardim de infância doce e divertida; Barney Stinson, um mulherengo incorrigível com um amor por ternos e estratégias para conquistar mulheres; e Robin Scherbatsky, uma jornalista canadense, durona por fora, mas com um coração mole por dentro. A jornada de Ted em busca do amor é o fio condutor da série, repleta de encontros e desencontros, relacionamentos que dão errado e lições aprendidas ao longo do caminho. Enquanto Ted busca a alma gêmea, seus amigos também vivenciam suas próprias histórias de amor, trabalho e amizade.",
+        link: "https://www.disneyplus.com/pt-br/series/foi-assim-que-aconteceu/3kpBeRQiKjkq",
+        tags: "mother how i met barney ted marshall robin lily"
+    },
+    {
+        titulo: "Prison Break ⭐⭐⭐⭐⭐ 4.9",
+        descricao: "Prison Break é uma série de televisão americana que prendeu a atenção do público com sua trama emocionante e repleta de suspense. A história gira em torno de Michael Scofield, um engenheiro civil que elabora um plano ousado para libertar seu irmão, Lincoln Burrows, injustamente condenado à pena de morte. A trama se desenrola em torno da prisão de Fox River, onde Lincoln está preso. Michael, para executar seu plano, se entrega à polícia e é enviado para a mesma prisão. Com a ajuda de desenhos tatuados em seu corpo, que funcionam como mapas e códigos, Michael manipula o sistema prisional e manipula os outros detentos para criar as condições perfeitas para a fuga.",
+        link: "https://www.disneyplus.com/pt-br/series/prison-break/7O2SFMVATNe5",
+        tags: "prison scofield michael lincoln"
+    }
+
+];
